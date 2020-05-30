50661ea830ad287e276d8c3b1cea671e98871646
Lorenzo <lzurzolo@gmail.com>
2020-05-30 18:03:18 -0400

    Added initial project files

---
+++ RenderSystem.cpp
@@ -0,0 +1,16 @@
+//
+// Created by lorenzo on 5/30/20.
+//
+
+#include "RenderSystem.hpp"
+
+RenderSystem::RenderSystem()
+= default;
+
+RenderSystem::~RenderSystem()
+= default;
+
+bool RenderSystem::Initialize()
+{
+    return false;
+}
---
+++ RenderSystem.hpp
@@ -0,0 +1,19 @@
+//
+// Created by lorenzo on 5/30/20.
+//
+
+#ifndef ZZRENDERER_RENDERSYSTEM_HPP
+#define ZZRENDERER_RENDERSYSTEM_HPP
+
+
+class RenderSystem
+{
+public:
+    RenderSystem();
+    ~RenderSystem();
+
+    bool Initialize();
+};
+
+
+#endif //ZZRENDERER_RENDERSYSTEM_HPP
